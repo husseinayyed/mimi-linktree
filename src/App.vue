75d26d327089c8e87f748a167353da87ebbff5e7
husseinayyed <honor9x.hasson@gmail.com>
2025-10-17 00:27:20 +0300

    Update 3: Added Social Media Balls, Animated Icons, Added new words for the description

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,27 @@ import gsap from 'gsap';
 import SplitText from 'gsap/SplitText';
 import { onMounted } from 'vue';
 import { desContext } from './context/arabic';
+import links from './context/links';
 onMounted(()=>{
   document.fonts.ready.then(()=>{
     gsap.registerPlugin(SplitText)
     const title = new SplitText("h1.title",{
       type:"chars"
     })
-    const des = new SplitText(".describe",{
-      type:"words"
-    })
+    
     const tl = gsap.timeline()
+    tl.from(".icon .container .bar",{
+      duration:1,
+      ease:"power2.inOut",
+      stagger:0.5,
+      opacity:0,
+      height:0
+    })
+    tl.from(".icon .container .after-element",{
+      duration:1,
+      ease:"power2.inOut",
+      opacity:0,
+    },"+=0.1")
     tl.from(title.chars,{
       opacity:0,
       duration:0.4,
@@ -20,32 +31,49 @@ onMounted(()=>{
       rotate:20,
       y:20,
       ease:"power1.out"
-    })
-    tl.from(des.words,{
+    },"-=0.4")
+    tl.from("p.describe",{
+      opacity:0,
+      duration:0.6,
+      x:5,
+      y:-5,
+      ease:"power1.in"
+    },"-=0.6")
+    tl.from(".social-media img",{
       opacity:0,
       duration:0.4,
       stagger:0.2,
-      y:20,
+      y:10,
+      x:10,
       ease:"power1.out"
-    })
+    },"+=0.2")
 
   })
 })
+function goLink(link) {
+  location.href = link
+}
 </script>
 
 <template>
   <main>
   <div class="icon">
     <div class="container">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="bar"></div>
+      <div class="after-element">S</div>
     </div>
   </div>
     <h1 class="quicksand-bold title">MimiStore</h1>
     <div v-for="text in desContext">
       <p class="amiri-regular describe rtl">{{ text }}</p>
+    </div>
+    <div class="social-media">
+      <div class="image-wrapper" :key="link.id" v-for="link in links">
+        <img :src="link.src" @click="goLink(link.link)" :alt="link.app">
+      </div>
     </div>
   </main>
   
@@ -65,11 +93,11 @@ main {
 }
 .describe {
   font-size: 1.28em;
-  display: inline;
+  display: block;
+  margin: 0;
 }
 .icon {
   overflow: hidden;
-  display: none;
   width: 80px;
   height: 80px;
   background-color: transparent;
@@ -82,25 +110,35 @@ main {
   justify-content: space-between;
   flex-flow: row nowrap;
 }
-.icon .container::after {
-  content: "S";
+.icon .after-element {
   position: absolute;
   top: -14%;
   left: 26%;
+  opacity: 1;
   font-size: 6rem;
   color: #00CFFF;
 }
-.icon .container div {
+.icon .container .bar {
   width: 8px;
   height: 80px;
   background-color: purple;
   border-radius: 4px;
 }
-.icon .container div:nth-child(2) {
+.icon .container .bar:nth-child(2) {
   transform: skewX(25deg);
   margin-right: 20px;
 }
-.icon .container div:nth-child(3) {
+.icon .container .bar:nth-child(3) {
   transform: skewX(-25deg);
+}
+.social-media {
+  display: flex;
+  gap: 10px;
+}
+img {
+  width:40px;
+  height:40px;
+  border-radius: 50%;
+  cursor: pointer;
 }
 </style>
---
+++ src/context/links.js
@@ -0,0 +1,16 @@
+import instagram from "../assets/instagram.png"
+import whatsapp from "../assets/whatsapp.png"
+const links = [
+    {
+        app:"instagram",
+        link:"https://www.instagram.com/mimi__store10",
+        src:instagram
+    },
+    {
+        app:"whatsapp",
+        link:"https://wa.me/9647737698219",
+        src:whatsapp
+    },
+
+]
+export default links
